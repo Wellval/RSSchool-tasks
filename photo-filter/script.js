b2df03bfb841d3fe9eae94edbbd4092938942291
Valentina Masalkova <vrassone@gmail.com>
2021-03-10 00:37:22 +0300

    fix: make more flexible input-output dom management

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,6 +1,7 @@
 const inputs = document.querySelectorAll('.filters input');
 const outputs = document.querySelectorAll('.filters output');
 const resetBtn = document.querySelector('.btn-reset');
+const loadBtn = document.querySelector('.btn-load--input');
 const labels = document.querySelectorAll('label');
 const fullscreen = document.querySelector('.fullscreen');
 const next = document.querySelector('.btn-next');
@@ -10,7 +11,7 @@ let hour = now.getHours();
 let dayTime;
 let n = 1;
 
-if ((hour => 6) && (hour < 12)) {
+if ((hour >= 6) && (hour < 12)) {
     dayTime = 'morning';
 } else if ((hour >= 12) && (hour < 18)) {
     dayTime = 'day';
@@ -22,8 +23,13 @@ function changeHandler() {
     let suffix = this.dataset.sizing || '';
 
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    let label = this.nextElementSibling;
-    label.innerHTML = this.value;
+    labels.forEach(label => {
+        for (let node of label.children) {
+            if (node.matches('output') && node.parentElement == this.parentElement) {
+                node.innerHTML = this.value;
+            }
+        }
+    });
 }
 
 function reset() {
@@ -35,15 +41,19 @@ function reset() {
         let label = input.nextElementSibling;
         label.innerHTML = input.value;
         document.documentElement.style.setProperty(`--${input.name}`, input.value + suffix);
-});
+    });
 }
 
 function nextPicture() {
-    if (n > 0 && n < 10) {
+    console.log(n)
+    if (n < 10) {
         picture.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${dayTime}/0${n++}.jpg`;
     } else if (n <= 20) {
         picture.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${dayTime}/${n++}.jpg`;
-    } else n = 1;
+    } else {
+        n = 1;
+        nextPicture();
+    }
 }
 
 inputs.forEach(input => input.addEventListener('mousemove', changeHandler));
@@ -54,3 +64,6 @@ fullscreen.addEventListener('click', () => {
     } else document.exitFullscreen();
 });
 next.addEventListener('click', nextPicture);
+loadBtn.addEventListener('load', () => {
+    console.log(this)
+})
